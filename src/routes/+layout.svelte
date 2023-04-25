f970aa3e42edbc9eb86d1c2f62b338c327075cb1
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-04-26 01:39:38 +0530

    Added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Booking from "$lib/booking.png";
+  import DeliteMenu from "$lib/delite-menu.png";
   import { page } from "$app/stores";
   import MainIcon from "$lib/food.png";
   $: RouteId = $page.route.id;
@@ -103,11 +105,11 @@
     </button>
   </a>
   <a
-    href="/products"
-    class={RouteId === "/products" ? "text-info active bg-[#0FBBFF3F]" : " "}
+    href="/bookings"
+    class={RouteId === "/bookings" ? "text-info active bg-[#0FBBFF3F]" : " "}
   >
     <button>
-      <svg
+      <!-- <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-5 w-5"
         fill="none"
@@ -119,15 +121,16 @@
           stroke-width="2"
           d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
         /></svg
-      >
+      > -->
+      <img src={Booking} alt="" class="w-8" />
     </button>
   </a>
   <a
-    href="/about"
-    class={RouteId === "/about" ? "text-info active bg-[#0FBBFF3F]" : " "}
+    href="/menu"
+    class={RouteId === "/menu" ? "text-info active bg-[#0FBBFF3F]" : " "}
   >
     <button>
-      <svg
+      <!-- <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-5 w-5"
         fill="none"
@@ -139,7 +142,8 @@
           stroke-width="2"
           d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
         /></svg
-      >
+      > -->
+      <img src={DeliteMenu} alt="" class="w-8" />
     </button>
   </a>
 </div>
