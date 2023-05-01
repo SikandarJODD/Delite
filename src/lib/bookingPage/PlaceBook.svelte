2d96792d814624a334ea49ca13da921f1cbcb19b
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-05-02 01:16:22 +0530

    Added stuff

--- src/lib/bookingPage/PlaceBook.svelte
+++ src/lib/bookingPage/PlaceBook.svelte
@@ -24,7 +24,7 @@
 </script>
 
 <main
-  class=" flex flex-col my-10 border-2 w-72 md:w-[500px] md:justify-between border-gray-900 rounded-lg p-5 bg-slate-200 mx-10"
+  class=" flex flex-col h-[350px] md:h-[250px] my-10 border-2 w-72 md:w-[500px] md:justify-between border-gray-900 rounded-lg p-5 bg-slate-200 mx-10"
 >
   <form class="text-left ml-3">
     <div class="my-3">
@@ -34,29 +34,28 @@
           required
           type="text"
           placeholder="Full Name"
-          class="input input-bordered input-accent w-44 max-w-xs"
+          class="input input-bordered input-accent w-48 max-w-xs"
           id="name"
           bind:value={name}
         />
       </label>
     </div>
-    <div class="my-3">
+    <div class="my-3 mintV">
       <label for="No.of customers">
         No.of customers :
         <input
           required
           type="text"
           placeholder="Type here"
-          class="input input-bordered input-accent w-40 max-w-xs"
+          class="input input-bordered input-accent w-48 max-w-xs"
           id="No.of customers"
           bind:value={numofCustomer}
         />
       </label>
     </div>
   </form>
-  <div>
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <form class="mt-5" on:click|preventDefault={bookTable}>
+  <div class="mt-5">
+    <form>
       <script
         src="https://checkout.razorpay.com/v1/payment-button.js"
         data-payment_button_id="pl_LkVt72J6TCpvVB"
@@ -65,12 +64,6 @@
       </script>
     </form>
   </div>
-  <!-- <div> -->
-  <!-- <img src={OrCode} alt="" class="h-[400px]" /> -->
-  <!-- <button class="btn btn-outline-success"
-      ><a href="https://rzp.io/i/hZQ9M0p7Kd">Payment</a></button
-    > -->
-  <!-- </div> -->
 </main>
 
 <style>
@@ -82,5 +75,8 @@
   input {
     color: rgba(0, 19, 61, 0.84) !important;
     margin-left: 10px;
+  }
+  .mintV{
+    visibility: visible !important;
   }
 </style>
