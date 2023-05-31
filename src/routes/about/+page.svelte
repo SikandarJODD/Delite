56223dc1eabb032bd36b65aa372da5aa8c38dbde
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2023-06-01 01:42:03 +0530

    Added about changes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -34,26 +34,30 @@
       >
     </p>
   </div>
-  
+
   <div class="bg-white">
     <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-      <div class="text-center ">
+      <div class="text-center">
         <div class=" rounded-full">
           <img
             src="https://media.licdn.com/dms/image/C4D03AQGP1dNhTLkZrA/profile-displayphoto-shrink_800_800/0/1662137583464?e=1691020800&v=beta&t=VsLUVmVvzVdtij0f-Qe5KjE_Vq4sYiyQ4Ov_8qkuF6w"
             alt=""
-            class="rounded-full h-52 w-52 mx-auto border-2 p-2  border-green-500 hover:scale-110 transition duration-300 ease-in-out mb-4"
+            class="rounded-full h-52 w-52 mx-auto border-2 p-2 border-green-500 hover:scale-110 transition duration-300 ease-in-out mb-4"
           />
         </div>
         <p
-          class="mt-1 text-xl font-extrabold text-gray-700  sm:text-5xl sm:tracking-tight lg:text-5xl"
+          class="mt-1 text-xl font-extrabold text-gray-700 sm:text-5xl sm:tracking-tight lg:text-5xl"
         >
-          Developed By <span class="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-sky-600"
+          Developed By <span
+            class="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-sky-600"
             >Sikandar.S.Bhide</span
           >
         </p>
         <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">
-          BunnyUI is a collection of UI components for SvelteKit and TailwindCSS.
+          Delite Restaurant is a web application developed using <span class="text-gray-700 font-mono bg-base-300 p-1 rounded-md"
+            >Sveltekit
+          </span>
+          & <span class="text-gray-700 font-mono bg-base-300 p-1 rounded-md">Supabase</span>
         </p>
       </div>
     </div>
